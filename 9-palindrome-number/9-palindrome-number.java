72de5a95318c730e5cd1e53c1eb67270a6a0988d
Ahmed Samir <pirloahmed339@gmail.com>
2022-05-20 05:29:31 +0200

    Time: 8 ms (93.54%), Space: 41.8 MB (84.43%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+       
+         if( x< 0 || (x !=0 && x % 10==0)){
+            return false;
+        }
+        
+        int reverseNumber = 0;
+        
+        while(x>reverseNumber){
+            reverseNumber= (reverseNumber * 10) + (x % 10);
+            x= x / 10;
+        
+        }
+        
+        return x==reverseNumber || x==reverseNumber / 10;
+        
+    }
+}
