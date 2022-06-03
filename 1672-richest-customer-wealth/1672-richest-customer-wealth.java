31a6807272ac5f8c5cd1c10d1916c5da03f94af6
Ahmed Samir <pirloahmed339@gmail.com>
2022-06-03 03:16:46 +0200

    Time: 1 ms (39.34%), Space: 43.6 MB (23.30%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int wealth = 0;
+        
+        for(int i = 0 ; i < accounts.length ; i++){
+            int sum = 0;
+            
+            for(int j = 0 ; j < accounts[i].length ; j++)
+            {
+                sum += accounts[i][j];
+            }
+             if(sum > wealth ){
+                 wealth = sum;
+             }
+                 
+        }
+        
+        return wealth;
+        
+    }
+}
