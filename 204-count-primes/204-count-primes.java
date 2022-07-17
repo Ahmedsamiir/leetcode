3fb9a44c38df5d1e67ee3a13681012a0111e0f2f
Ahmed Samir <pirloahmed339@gmail.com>
2022-07-17 22:05:42 +0200

    Time: 179 ms (56.19%), Space: 68.8 MB (27.83%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -24,5 +24,35 @@ class Solution {
  
 	return count;
         
+//           n = n-1;
+ 
+//     ArrayList<Integer> primes = new ArrayList<Integer>();
+ 
+//     if(n<=1) 
+//         return 0;
+//     if(n==2)
+//         return 1;
+//     if(n==3)
+//         return 2;
+ 
+//     primes.add(2);
+//     primes.add(3);
+ 
+//     for(int i=4; i<=n; i++){
+//         boolean isPrime = true;
+//         for(int p: primes){
+//             int m = i%p;
+//             if(m==0){
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+ 
+//         if(isPrime){
+//             primes.add(i);
+//         }
+//     }
+ 
+//     return primes.size();
     }
 }
