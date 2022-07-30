b3b87fa78e00979827e0aaa6bae0bc12bc17c0aa
Ahmed Samir <pirloahmed339@gmail.com>
2022-07-30 03:25:27 +0200

    Time: 1 ms (89.28%), Space: 43.6 MB (33.59%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        rec(res, new ArrayList<>(), nums, 0, used);
+        
+        return res;
+
+        
+    }
+    
+     private void rec(List<List<Integer>> res, List<Integer> temp, int[] nums, int idx, boolean[] used){
+        
+        res.add(new ArrayList<>(temp));
+        if(temp.size() == nums.length){
+            return;
+        }
+        
+        for(int i = idx; i < nums.length; i++){
+            if(used[i]){
+                continue;
+            }
+            temp.add(nums[i]);
+            used[i] = true;
+            rec(res, temp, nums, i, used);
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
